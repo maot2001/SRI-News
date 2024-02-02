dd6665110faf4b1f527fd8496176357f5b08e2b2
maot2001 <106125010+maot2001@users.noreply.github.com>
2024-02-02 16:41:27 -0500

    feat basic Lab-1
    An spacy pre-processing SRI implementation with embedding in gensim tf-idf model

--- src/code/imp-spacy.py
+++ src/code/imp-spacy.py
@@ -2,22 +2,37 @@ import ir_datasets
 import spacy
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
-from gensim.matutils import corpus2dense
-import numpy as np
 
-nlp = spacy.load("en_core_web_sm")
-
-dataset = ir_datasets.load("cranfield")
-
-documents = [doc.text for doc in dataset.docs_iter()]
+dictionary = {}
 
 def tokenization_spacy(texts):
-  tokenized_texts = []
-  for text in texts:
-    doc = nlp(text)
-    tokens = [(token.text, token.lemma_, token.pos_) for token in doc if not token.is_space and not token.is_punct and \
-               not token.is_stop and not token.is_digit]
-    tokenized_texts.append(tokens)
-  return tokenized_texts
+  return [[token.text for token in nlp(doc) if token.is_alpha and not token.is_stop] 
+          for doc in texts]
 
-tokenized_docs = tokenization_spacy(documents[:10])
+def filter_tokens_by_occurrence(tokenized_docs, no_below=2, no_above=0.5):
+  global dictionary
+  dictionary = Dictionary(tokenized_docs)
+  dictionary.filter_extremes(no_below=no_below, no_above=no_above)
+
+  filtered_words = [word for _, word in dictionary.iteritems()]
+  filtered_tokens = [
+      [word for word in doc if word in filtered_words]
+      for doc in tokenized_docs
+  ]
+
+  return filtered_tokens
+
+def build_vocabulary(dictionary):
+  return list(dictionary.token2id.keys())
+
+def vector_representation(tokenized_docs, dictionary):
+  model_tfidf = TfidfModel(dictionary=dictionary, normalize=True)
+  return [model_tfidf[dictionary.doc2bow(tokens)] for tokens in tokenized_docs]
+
+nlp = spacy.load("en_core_web_sm")
+dataset = ir_datasets.load("cranfield")
+documents = [doc.text for doc in dataset.docs_iter()]
+
+tokenized_docs = filter_tokens_by_occurrence(tokenization_spacy(documents[:10]))
+vocabulary = build_vocabulary(dictionary)
+vector_repr = vector_representation(tokenized_docs, dictionary)
