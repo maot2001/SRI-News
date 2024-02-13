75d891e60f394f8dd4f0886cfd88ea9b32206e89
Jan Carlos Pérez González <wwjancarlospg2000@gmail.com>
2024-02-13 03:01:14 -0500

    Get_matching_docs DONE

--- src/code/lab2.py
+++ src/code/lab2.py
@@ -1,14 +1,9 @@
 import gensim
 import spacy
-from sympy import sympify, to_dnf, Not, And, Or
+from sympy import sympify,simplify,  to_dnf, Not, And, Or, logic
 import nltk
 import ir_datasets
 
-nlp = spacy.load("en_core_web_sm")
-
-
-dataset = ir_datasets.load("cranfield")
-documents = [doc.text for doc in dataset.docs_iter()]
 
 def tokenization_spacy(texts):
   return [[token for token in nlp(doc) if token.is_alpha and not token.is_stop] 
@@ -31,14 +26,13 @@ def morphological_reduction_spacy(tokenized_docs, use_lemmatization=True):
     for doc in tokenized_docs
   ]
 
-def convert_to_text(tokenized_docs):
-  return[
-    [token.text for token in doc] for doc in tokenized_docs
-  ]
-
 def query_to_dnf(query):
     processed_query = ""
-    q = query.split(" ")
+    try:
+        q = query.split(" ")
+    except ex:
+        q = [query]
+    #Se recorre cada elemento de la query y se parsea a un simbolo reconocido
     for i in q:
         if i == "AND":
             processed_query=processed_query + " & "
@@ -48,13 +42,97 @@ def query_to_dnf(query):
             processed_query =processed_query + " ~ "
         else:
             processed_query = processed_query + " " + i
-    
+    #Se lleva la query a una expresion sympify para poder trabajar con ella
     query_expr = sympify(processed_query, evaluate=False)
+    #Se lleva a forma normal disyunitiva
     query_dnf = to_dnf(query_expr, simplify=True)
 
     return query_dnf
 
+def get_clean_query(query):
+    q = query_to_dnf(query)
+    # hace una especie de interseccion de la query para no repetir
+    # en la documentacion los desarrolladores no saben bien que hace esto(y son seniors), 
+    # solo que hace las expresiones mas simples
+    clean_q = simplify(q)
+    # .args me devuelve una tupla con los implicados en el or(si es un or, como debe ser) 
+    tup = clean_q.args
+    res = [] 
+    # deberia quedar cada forma conjuntiva en una tupla
+    if not type(clean_q) == Or:
+        res.append(tup)
+        return res
+    for s in tup:
+        if type(s)==And:
+            res.append(s.args)
+        else: 
+            res.append(s)
+    return res
+
+def get_matching_docs(query_dnf):
+    """Por cada documento analizo las queries y si se cumple los agrego a una lista
+
+    Args:
+        query_dnf (): _Es una lista con distintos tipos, pueden ser símbolos de sympy o tuplas de símbolos de sympy_
+
+    Returns:
+        _List<List<String>>_: _La lista de todos los documentos d_i tales que sim(d_i, query_dnf)=1_
+    """
+    global tokenized_docs, dictionary, corpus
+    matching_documents = []
+    for doc in tokenized_docs:
+        for i in query_dnf:
+            if type(i) == tuple:
+                success = True
+                aux = False
+                for j in i:
+                    is_not = False
+                    if type(j) == Not:
+                        is_not = True
+                        w = str(j.args[0])
+                    else: w = str(j)
+                    contains = w in doc
+                    if is_not and not contains:
+                        aux = True
+                        success = success and not contains
+                    elif not is_not and contains:
+                        aux = True
+                        success = success and contains
+                if success and aux:
+                    matching_documents.append(doc)
+                    break
+            else:
+                is_not = False
+                if type(i) == Not:
+                    is_not = True
+                    w = str(i.args[0])
+                else: w = str(i)
+                contains = w in doc
+                if is_not and not contains:
+                    matching_documents.append(doc)
+                    break
+                elif not is_not and contains:
+                    matching_documents.append(doc)
+                    break
+    return matching_documents
+
+
+
+nlp = spacy.load("en_core_web_sm")
+dataset = ir_datasets.load("cranfield")
+documents = [doc.text for doc in dataset.docs_iter()]
 tokenized_docs = morphological_reduction_spacy(remove_stopwords_spacy(remove_noise_spacy(tokenization_spacy(documents))), True)
+dictionary = gensim.corpora.Dictionary(tokenized_docs)
+vocabulary = list(dictionary.token2id.keys())
+corpus = [dictionary.doc2bow(doc) for doc in tokenized_docs]
+
+#Test
+query = "( experimental AND investigation AND ( aerodynamic ) OR NOT simple ) "
+print(query_to_dnf(query))
+query = get_clean_query(query)
+print(query)
+x = get_matching_docs(query)
+print(len(x))
 
 
 dictionary = gensim.corpora.Dictionary(tokenized_docs)
